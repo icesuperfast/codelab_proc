9ad9fb7893ff6363f023361a4262f95f75b816b4
icesuperfast <icezanarak011@gmail.com>
2022-10-02 02:13:46 +0700

    work

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 from .views import (
-    list_view,   
+    list_view,
+    client #new function in  views.py    
 )
 
 app_name = 'main'
 urlpatterns = [
     path('', list_view, name='home-list'),
-
+    path('join-now',client, name='hangout'),#new url route
 ]
